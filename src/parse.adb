bac1d3118a19f80fd50f0c5e5556b31435e09fa3
Martin Keegan <martin@no.ucant.org>
2015-06-16 01:10:08 +0100

    reindent

--- src/parse.adb
+++ src/parse.adb
@@ -223,36 +223,35 @@ procedure parse(command_line : string := "") is
    procedure tricks_enclitic(input_word : string;
                              entering_trpa_last : in out integer;
                              have_done_enclitic : boolean) is
-               try : constant string := lower_case(input_word);
-            begin
-               if have_done_enclitic  then    return;   end if;
+      try : constant string := lower_case(input_word);
+   begin
+      if have_done_enclitic  then    return;   end if;
+      
+      entering_trpa_last := trpa_last;
+  loop_over_enclitic_tackons:
+      for i in 1..4  loop   --  que, ne, ve, (est)
 
-               entering_trpa_last := trpa_last;
-           loop_over_enclitic_tackons:
-               for i in 1..4  loop   --  que, ne, ve, (est)
-
-              remove_a_tackon:
-                  declare
-                     less : constant string :=
-                       subtract_tackon(try, tackons(i));
-                  begin
-                     if less  /= try  then       --  LESS is less
-                        try_tricks(less, trpa, trpa_last, line_number, word_number);
-                        
-                        if trpa_last > entering_trpa_last  then      --  have a possible word
-                           trpa_last := trpa_last + 1;
-                           trpa(entering_trpa_last+2..trpa_last) :=
-                             trpa(entering_trpa_last+1..trpa_last-1);
-                           trpa(entering_trpa_last+1) := (tackons(i).tack,
-                             ((tackon, null_tackon_record), 0, null_ending_record, x, x),
-                             addons, dict_io.count(tackons(i).mnpc));
-                        end if;
-                        exit loop_over_enclitic_tackons;
-                     end if;
-                  end remove_a_tackon;
-               end loop loop_over_enclitic_tackons;
-            end tricks_enclitic;
-   
+     remove_a_tackon:
+         declare
+            less : constant string :=
+              subtract_tackon(try, tackons(i));
+         begin
+            if less  /= try  then       --  LESS is less
+               try_tricks(less, trpa, trpa_last, line_number, word_number);
+               
+               if trpa_last > entering_trpa_last  then      --  have a possible word
+                  trpa_last := trpa_last + 1;
+                  trpa(entering_trpa_last+2..trpa_last) :=
+                    trpa(entering_trpa_last+1..trpa_last-1);
+                  trpa(entering_trpa_last+1) := (tackons(i).tack,
+                    ((tackon, null_tackon_record), 0, null_ending_record, x, x),
+                    addons, dict_io.count(tackons(i).mnpc));
+               end if;
+               exit loop_over_enclitic_tackons;
+            end if;
+         end remove_a_tackon;
+      end loop loop_over_enclitic_tackons;
+   end tricks_enclitic;
    
    procedure parse_line(input_line : string) is
       l : integer := trim(input_line)'last;
