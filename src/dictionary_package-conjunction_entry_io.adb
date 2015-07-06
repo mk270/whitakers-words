cb1c71e84d339ec9b09667c46c8d21cea83ba129
darkestkhan <darkestkhan@gmail.com>
2015-07-06 14:44:30 +0000

    Change pragma Warnings ({Off,On}) to Unreferenced.
    Pragma Warnings (Off) turns off all warnings, including warnings that may be
    other than unreferenced argument, thus it is better to use Unreferenced which
    suppresses only particular warnings on particular argument.
    
    Signed-off-by: darkestkhan <darkestkhan@gmail.com>

--- src/dictionary_package-conjunction_entry_io.adb
+++ src/dictionary_package-conjunction_entry_io.adb
@@ -23,9 +23,8 @@ package body Conjunction_Entry_IO is
 
    ---------------------------------------------------------------------------
 
-   pragma Warnings (Off, "formal parameter ""File"" is not referenced");
    procedure Get (File : in File_Type; Item : out Conjunction_Entry) is
-      pragma Warnings (On, "formal parameter ""File"" is not referenced");
+      pragma Unreferenced (File);
    begin
       Item := Null_Conjunction_Entry;
    end Get;
@@ -61,9 +60,8 @@ package body Conjunction_Entry_IO is
 
    ---------------------------------------------------------------------------
 
-   pragma Warnings (Off, "formal parameter ""Item"" is not referenced");
    procedure Put (Target : out String; Item : in Conjunction_Entry) is
-      pragma Warnings (On, "formal parameter ""Item"" is not referenced");
+      pragma Unreferenced (Item);
    begin
       Target (Target'First .. Target'Last) := (others => ' ');
    end Put;
