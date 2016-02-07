feb15d6670eb4207a84d28f32a67de23f5c07ea6
Martin Keegan <martin@no.ucant.org>
2016-02-07 19:06:54 +0000

    fix missing config file

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -7,6 +7,17 @@ set -eu
 
 cd $(dirname $0)/..
 
+cleanup () {
+    local exit_val=$?
+    rm -f WORD.MDV
+    exit $exit_val
+}
+
+if [ ! -f WORD.MDV ]; then
+    cp test/WORD.MDV_template WORD.MDV
+    trap cleanup EXIT
+fi
+
 bin/words 'rem acu tetigisti' | diff -q -- - test/expected.txt
 
 bin/words < test/aeneid_bk4.txt | diff -q -- - test/aeneid_bk4.expected
