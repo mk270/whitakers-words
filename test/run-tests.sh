651034cc46ee4a8a3f6971d27c17ab6ca6656acd
Martin Keegan <martin@no.ucant.org>
2016-02-07 18:12:00 +0000

    update tests

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -3,6 +3,12 @@
 # This script runs an absolutely trivial smoke test; we'll replace it
 # with something better in due course
 
+set -eu
+
 cd $(dirname $0)/..
 
 bin/words 'rem acu tetigisti' | diff -q -- - test/expected.txt
+
+bin/words < test/aeneid_bk4.txt | diff -q -- - test/aeneid_bk4.expected
+
+echo PASS
