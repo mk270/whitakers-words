78629b65068b733295b50db6660d26315b5d56f9
Martin Keegan <martin@no.ucant.org>
2016-03-19 14:19:41 +0000

    add pessimistic debug line

--- src/words_engine/words_engine-parse.adb
+++ src/words_engine/words_engine-parse.adb
@@ -1052,6 +1052,7 @@ is
             if S /= "" and S /= "." then
                Put_Line (Standard_Error, "Appending Word: [" & S & "]");
                Words.Append (To_Unbounded_String (S));
+               Put_Line (Standard_Error, "Appended Word: [" & S & "]");
             end if;
          end;
       end loop;
