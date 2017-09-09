9f2afaf994ae60cbebaec88c11dd06419bcf1386
Martin Keegan <martin@no.ucant.org>
2017-09-09 23:17:56 +0100

    reindent

--- src/words_engine/words_engine-list_package.adb
+++ src/words_engine/words_engine-list_package.adb
@@ -616,63 +616,63 @@ package body Words_Engine.List_Package is
       Osra          : Stem_Inflection_Array (1 .. Stem_Inflection_Array_Size)
            := Null_Sra;
    begin
-         pragma Assert (WA.Dict'First = WA.Stem_IAA'First);
+      pragma Assert (WA.Dict'First = WA.Stem_IAA'First);
 
-         Output_Loop :
-         for J in WA.Dict'Range loop
-            declare
-               Sra : constant Stem_Inflection_Array  := WA.Stem_IAA (J);
-               DER : constant Dictionary_MNPC_Record := WA.Dict (J);
+      Output_Loop :
+      for J in WA.Dict'Range loop
+         declare
+            Sra : constant Stem_Inflection_Array  := WA.Stem_IAA (J);
+            DER : constant Dictionary_MNPC_Record := WA.Dict (J);
+         begin
+            -- hack to work around static/dynamic schizophrenia
+            if DER = Null_Dictionary_MNPC_Record then
+               exit Output_Loop;
+            end if;
+
+            --  Skips one identical SRA no matter what comes next
+            if Sra /= Osra  then
+
+               Put_Inflection_Array_J :
+               for K in Sra'Range loop
+                  exit Put_Inflection_Array_J when Sra (K) =
+                    Null_Stem_Inflection_Record;
+
+                  Put_Inflection (Configuration, Output, Sra (K),
+                    DER);
+                  if Sra (K).Stem (1 .. 3) = "PPL"  then
+                     Ada.Text_IO.Put_Line (Output,
+                                           Head (Xp.Ppp_Meaning, Mm));
+                  end if;
+               end loop Put_Inflection_Array_J;
+               Osra := Sra;
+            end if;
+
+            Putting_Form :
             begin
-               -- hack to work around static/dynamic schizophrenia
-               if DER = Null_Dictionary_MNPC_Record then
-                  exit Output_Loop;
+               if J = WA.Dict'First or else
+                 Support_Utils.Dictionary_Form (DER.De) /=
+                 Support_Utils.Dictionary_Form (WA.Dict (J - 1).De)
+               then
+                  --  Put at first chance, skip duplicates
+                  Put_Form (Output, Sra (1), DER);
                end if;
+            end Putting_Form;
 
-               --  Skips one identical SRA no matter what comes next
-               if Sra /= Osra  then
-
-                  Put_Inflection_Array_J :
-                  for K in Sra'Range loop
-                     exit Put_Inflection_Array_J when Sra (K) =
-                       Null_Stem_Inflection_Record;
-
-                     Put_Inflection (Configuration, Output, Sra (K),
-                       DER);
-                     if Sra (K).Stem (1 .. 3) = "PPL"  then
-                        Ada.Text_IO.Put_Line (Output,
-                                              Head (Xp.Ppp_Meaning, Mm));
-                     end if;
-                  end loop Put_Inflection_Array_J;
-                  Osra := Sra;
+            Putting_Meaning :
+            begin
+               if DER.D_K not in General .. Unique or else (
+                 J + 1 > WA.Dict'Last or else
+                    DER.De.Mean /= WA.Dict (J + 1).De.Mean)
+               then
+                  --  Handle simple multiple MEAN with same IR and FORM
+                  --  by anticipating duplicates and waiting until change
+                  Put_Meaning_Line (Output, Sra (1), DER, Mm, Xp);
                end if;
+            end Putting_Meaning;
 
-               Putting_Form :
-               begin
-                  if J = WA.Dict'First or else
-                    Support_Utils.Dictionary_Form (DER.De) /=
-                    Support_Utils.Dictionary_Form (WA.Dict (J - 1).De)
-                  then
-                     --  Put at first chance, skip duplicates
-                     Put_Form (Output, Sra (1), DER);
-                  end if;
-               end Putting_Form;
-
-               Putting_Meaning :
-               begin
-                  if DER.D_K not in General .. Unique or else (
-                    J + 1 > WA.Dict'Last or else
-                       DER.De.Mean /= WA.Dict (J + 1).De.Mean)
-                  then
-                     --  Handle simple multiple MEAN with same IR and FORM
-                     --  by anticipating duplicates and waiting until change
-                     Put_Meaning_Line (Output, Sra (1), DER, Mm, Xp);
-                  end if;
-               end Putting_Meaning;
-
-               Do_Pause (Output, WA.I_Is_Pa_Last);
-            end;
-         end loop Output_Loop;
+            Do_Pause (Output, WA.I_Is_Pa_Last);
+         end;
+      end loop Output_Loop;
    end Put_Parse_Details;
 
    procedure Fix_Adverb
