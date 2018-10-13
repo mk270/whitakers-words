6867b5565684bb545c473f68ff8f1408cf953630
Martin Keegan <martin@no.ucant.org>
2018-10-13 22:53:26 +0100

    move more tables into own package

--- src/words_engine/words_engine-trick_tables.ads
+++ src/words_engine/words_engine-trick_tables.ads
@@ -52,4 +52,57 @@ package Words_Engine.Trick_Tables is
 
    function Get_Tricks_Table (C : Character) return TricksT;
 
+   Any_Tricks : constant TricksT := (
+     (Max => 0, Op => TC_Internal, I1 => +"ae", I2 => +"e"),
+     (Max => 0, Op => TC_Internal, I1 => +"bul", I2 => +"bol"),
+     (Max => 0, Op => TC_Internal, I1 => +"bol", I2 => +"bul"),
+     (Max => 0, Op => TC_Internal, I1 => +"cl", I2 => +"cul"),
+     (Max => 0, Op => TC_Internal, I1 => +"cu", I2 => +"quu"),
+     (Max => 0, Op => TC_Internal, I1 => +"f", I2 => +"ph"),
+     (Max => 0, Op => TC_Internal, I1 => +"ph", I2 => +"f"),
+     (Max => 0, Op => TC_Internal, I1 => +"h", I2 => +""),
+     (Max => 0, Op => TC_Internal, I1 => +"oe", I2 => +"e"),
+     (Max => 0, Op => TC_Internal, I1 => +"vul", I2 => +"vol"),
+     (Max => 0, Op => TC_Internal, I1 => +"vol", I2 => +"vul"),
+     (Max => 0, Op => TC_Internal, I1 => +"uol", I2 => +"vul")
+  );
+
+   Mediaeval_Tricks : constant TricksT := (
+     --  Harrington/Elliott    1.1.1
+     (Max => 0, Op => TC_Internal, I1 => +"col", I2 => +"caul"),
+     --  Harrington/Elliott    1.3
+     (Max => 0, Op => TC_Internal, I1 => +"e", I2 => +"ae"),
+     (Max => 0, Op => TC_Internal, I1 => +"o", I2 => +"u"),
+     (Max => 0, Op => TC_Internal, I1 => +"i", I2 => +"y"),
+     --  Harrington/Elliott    1.3.1
+     (Max => 0, Op => TC_Internal, I1 => +"ism", I2 => +"sm"),
+     (Max => 0, Op => TC_Internal, I1 => +"isp", I2 => +"sp"),
+     (Max => 0, Op => TC_Internal, I1 => +"ist", I2 => +"st"),
+     (Max => 0, Op => TC_Internal, I1 => +"iz", I2 => +"z"),
+     (Max => 0, Op => TC_Internal, I1 => +"esm", I2 => +"sm"),
+     (Max => 0, Op => TC_Internal, I1 => +"esp", I2 => +"sp"),
+     (Max => 0, Op => TC_Internal, I1 => +"est", I2 => +"st"),
+     (Max => 0, Op => TC_Internal, I1 => +"ez", I2 => +"z"),
+     --  Harrington/Elliott    1.4
+     (Max => 0, Op => TC_Internal, I1 => +"di", I2 => +"z"),
+     (Max => 0, Op => TC_Internal, I1 => +"f", I2 => +"ph"),
+     (Max => 0, Op => TC_Internal, I1 => +"is", I2 => +"ix"),
+     (Max => 0, Op => TC_Internal, I1 => +"b", I2 => +"p"),
+     (Max => 0, Op => TC_Internal, I1 => +"d", I2 => +"t"),
+     (Max => 0, Op => TC_Internal, I1 => +"v", I2 => +"b"),
+     (Max => 0, Op => TC_Internal, I1 => +"v", I2 => +"f"),
+     (Max => 0, Op => TC_Internal, I1 => +"v", I2 => +"f"),
+     (Max => 0, Op => TC_Internal, I1 => +"s", I2 => +"x"),
+     --  Harrington/Elliott    1.4.1
+     (Max => 0, Op => TC_Internal, I1 => +"ci", I2 => +"ti"),
+     --  Harrington/Elliott    1.4.2
+     (Max => 0, Op => TC_Internal, I1 => +"nt", I2 => +"nct"),
+     (Max => 0, Op => TC_Internal, I1 => +"s", I2 => +"ns"),
+     --  Others
+     (Max => 0, Op => TC_Internal, I1 => +"ch", I2 => +"c"),
+     (Max => 0, Op => TC_Internal, I1 => +"c", I2 => +"ch"),
+     (Max => 0, Op => TC_Internal, I1 => +"th", I2 => +"t"),
+     (Max => 0, Op => TC_Internal, I1 => +"t", I2 => +"th")
+  );
+
 end Words_Engine.Trick_Tables;
