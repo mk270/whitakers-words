9319015e7668fe3573a88eaf20a916b0d5243d13
Martin Keegan <martin@no.ucant.org>
2019-02-11 21:16:49 +0000

    add python conventional doxology

--- scripts/dictline_csv.py
+++ scripts/dictline_csv.py
@@ -20,13 +20,17 @@ FIELD_IDX = (0, 19, 38, 57, 76, 83, 95, 100, 110)
 INFILE = 'DICTLINE.GEN'
 OUTFILE = 'DICTLINE.GEN.csv'
 
-with open(INFILE, encoding="latin-1") as dictline, \
-     open(OUTFILE, 'w', encoding="utf-8") as dictline_csv:
+def run():
+    with open(INFILE, encoding="latin-1") as dictline, \
+         open(OUTFILE, 'w', encoding="utf-8") as dictline_csv:
 
-    writer = csv.writer(dictline_csv, delimiter='\t', lineterminator="\n")
-    writer.writerow(TITLES)
+        writer = csv.writer(dictline_csv, delimiter='\t', lineterminator="\n")
+        writer.writerow(TITLES)
 
-    for id_, l in enumerate(dictline, 1):
-        fields = [l[start:end].strip()
-                  for start, end in zip(FIELD_IDX, FIELD_IDX[1:] + (None,))]
-        writer.writerow([id_] + fields)
+        for id_, l in enumerate(dictline, 1):
+            fields = [l[start:end].strip()
+                      for start, end in zip(FIELD_IDX, FIELD_IDX[1:] + (None,))]
+            writer.writerow([id_] + fields)
+
+if __name__ == '__main__':
+    run()
